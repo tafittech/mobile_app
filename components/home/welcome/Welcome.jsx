32b71deafe3ce0061f1b93bccf1d796bcddc523d
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-07-01 14:35:01 -0400

    second commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native';
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { icons, SIZES, } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Trevor Jemmott</Text>
+        <Text style={styles.welcomeMessage}>what job you have today? </Text>
+
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+
+        </View>
+
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
